8156303472d330b4231ba2491641917a80ae0bb9
Artyom Cheremisin <art.cheremisin@gmail.com>
2021-09-26 15:17:45 +0300

    added ESLint

--- lesson_4/task_4/index.js
+++ lesson_4/task_4/index.js
@@ -1,8 +1,16 @@
 function run() {
-    var a = 0,
-        b = 1;
-
-    if (a == b) {
-        return c;
-    }
-}
+	var a = 0;
+  
+	if (a == 1) {
+	  return c;
+	}
+  
+	for (let i = 1; i < 10; i++) {
+	  console.log(i);
+	}
+  
+	return (d = a);
+  }
+  
+  run();
+  
