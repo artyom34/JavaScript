285895a13a449dcb134f157478ffcfbce44531a6
Artyom Cheremisin <art.cheremisin@gmail.com>
2021-10-14 10:02:21 +0300

    converting numbers

--- lesson_10/task_2/index.js
+++ lesson_10/task_2/index.js
@@ -1,39 +1,25 @@
 'use strict';
 
-function compareObjects(obj1, obj2) {
-// put your code here	
-	const objData1 = Object.entries(obj1).flat(Infinity);
-	const objData2 = Object.entries(obj2).flat(Infinity);
+const numbersArray = [12.3, 11, 4.4, 10.2, 15, '12  ', ' 12a'];
 
-	
+function getParsedIntegers(array){
 
-	return true;
+	return array.map(number => Number.parseInt(number));
+}
+function getParsedIntegersV2(array){
+
+	return array.map(number => parseInt(number));
+}
+function getParsedFloats(array){
+
+	return array.map(number => Number.parseFloat(number));
+}
+function getParsedFloatsV2(array){
+
+	return array.map(number => parseFloat(number));
 }
 
-// examples
-const obj1 = {
-name: 'Tom',
-age: 17,
-};
-
-const obj2 = {
-name: 'Bob',
-age: 17,
-};
-
-const obj3 = {
-name: 'Bob',
-age: 17,
-student: false,
-};
-
-const obj4 = {
-name: 'Tom',
-age: 17,
-};
-
-// console.log(compareObjects(obj1, obj2)); // ==> false
-// compareObjects(obj2, obj3); // ==> false
-// console.log(compareObjects(obj1, obj4)); // ==> true
-
-console.log(compareObjects(obj1, obj2));
+console.log(getParsedIntegers(numbersArray));
+console.log(getParsedIntegersV2(numbersArray));
+console.log(getParsedFloats(numbersArray));
+console.log(getParsedFloatsV2(numbersArray));
