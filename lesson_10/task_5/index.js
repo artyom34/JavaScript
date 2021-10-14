4dc95e209794e90fcafe5b7e83c1fe36e208c842
Artyom Cheremisin <art.cheremisin@gmail.com>
2021-10-14 13:39:43 +0300

    sum of fractional numbers

--- lesson_10/task_5/index.js
+++ lesson_10/task_5/index.js
@@ -1,39 +1,12 @@
 'use strict';
 
-function compareObjects(obj1, obj2) {
-// put your code here	
-	const objData1 = Object.entries(obj1).flat(Infinity);
-	const objData2 = Object.entries(obj2).flat(Infinity);
+const numbersArray = [2, 12.4, 20, 12, 34.528];
 
+function getTotalPrice(array){
+
+	let sum = array.reduce((sum, current) => sum + current);
 	
-
-	return true;
+	return "$" + Math.trunc(sum * 100) / 100;
 }
 
-// examples
-const obj1 = {
-name: 'Tom',
-age: 17,
-};
-
-const obj2 = {
-name: 'Bob',
-age: 17,
-};
-
-const obj3 = {
-name: 'Bob',
-age: 17,
-student: false,
-};
-
-const obj4 = {
-name: 'Tom',
-age: 17,
-};
-
-// console.log(compareObjects(obj1, obj2)); // ==> false
-// compareObjects(obj2, obj3); // ==> false
-// console.log(compareObjects(obj1, obj4)); // ==> true
-
-console.log(compareObjects(obj1, obj2));
+console.log(getTotalPrice(numbersArray));
