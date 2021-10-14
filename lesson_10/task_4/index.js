415a5eef3e1906a0fd9df065478a1403b6a85675
Artyom Cheremisin <art.cheremisin@gmail.com>
2021-10-14 11:57:28 +0300

    maximum absolute number

--- lesson_10/task_4/index.js
+++ lesson_10/task_4/index.js
@@ -1,39 +1,20 @@
 'use strict';
 
-function compareObjects(obj1, obj2) {
-// put your code here	
-	const objData1 = Object.entries(obj1).flat(Infinity);
-	const objData2 = Object.entries(obj2).flat(Infinity);
-
-	
-
-	return true;
-}
+const getMaxAbsoluteNumber = arr => {
+	// put your code here
+	if(Array.isArray(arr))
+		if(arr.length > 0){
+			return Math.max(...arr.map(number => Math.abs(number)));
+		}
+		else {
+			return null;
+		}
+	return null;
+};
 
 // examples
-const obj1 = {
-name: 'Tom',
-age: 17,
-};
-
-const obj2 = {
-name: 'Bob',
-age: 17,
-};
-
-const obj3 = {
-name: 'Bob',
-age: 17,
-student: false,
-};
-
-const obj4 = {
-name: 'Tom',
-age: 17,
-};
-
-// console.log(compareObjects(obj1, obj2)); // ==> false
-// compareObjects(obj2, obj3); // ==> false
-// console.log(compareObjects(obj1, obj4)); // ==> true
-
-console.log(compareObjects(obj1, obj2));
+console.log(getMaxAbsoluteNumber([-10, 10, -10])); // ===> 10
+getMaxAbsoluteNumber([2.1, 0, 1.6]); // ===> 2.1
+getMaxAbsoluteNumber([-6, 3, 5, -1]); // ===> 6
+console.log(getMaxAbsoluteNumber([-777, 3, -1, 45, -20])); // ===> 777
+console.log(getMaxAbsoluteNumber([]));
