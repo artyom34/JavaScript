47818ef91497cff475f1fa9b099f230fd4e14419
Artyom Cheremisin <art.cheremisin@gmail.com>
2021-10-20 20:36:01 +0300

    Array of random numbers

--- lesson_10/task_6/index.js
+++ lesson_10/task_6/index.js
@@ -4,18 +4,19 @@ const getRandomNumbers = (length, from, to) => {
 	// put your code here
 	let numbers = [];
 
-	if(length > 0){
+		if((to - from) < 1 && (to - from) > -1){
+			return null;
+		}
+
 		for(let i = 0; i < length; i++){
-			numbers[i] = Number.parseInt(Math.random() * (to - from) + from);
-		} 
+			numbers[i] = parseInt(Math.random()
+			* (to - from) + from);
+		}
+
 		return numbers;
-	}
-	else{
-		return null
-	}
 };
 
 // examples
-console.log(getRandomNumbers(5, 1.4, 3.22)); // ==> [2, 2, 2, 3, 2]
+console.log(getRandomNumbers(5, 1.4, 3.6)); // ==> [2, 2, 2, 3, 2]
 getRandomNumbers(5, 1.4, 3.22); // ==> [3, 2, 2, 2, 2]
 getRandomNumbers(5, 1.4, 3.22); // ==> [3, 3, 2, 3, 2]
