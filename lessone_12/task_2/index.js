1ec8ff9a435be5e2a2f6b8566b4e70657408363f
Artyom Cheremisin <art.cheremisin@gmail.com>
2021-10-21 20:29:28 +0300

    reverse String

---
+++ lessone_12/task_2/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function reverseString(string){
+	
+	if(typeof string !== 'string'){
+		return null;
+	}
+	
+	let reverseWord = '';
+	let arrayLetters = string.split('');
+
+	arrayLetters.reverse();
+	
+	for(let i = 0; i < arrayLetters.length; i++){
+			reverseWord = reverseWord + arrayLetters[i];
+	}
+	return reverseWord;
+}
+
+console.log(reverseString('asdf'));
