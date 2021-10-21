7060b8dce1348f80a375f50482f6fc61267b0f97
Artyom Cheremisin <art.cheremisin@gmail.com>
2021-10-21 19:44:17 +0300

    calc

---
+++ lessone_12/task_1/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function calc(expression){
+
+	let result = 0;
+	let [firstNumber, opertor, secondNumber] = 
+		expression.split(' ');
+
+	switch(opertor){
+		case '+':  result = +firstNumber + +secondNumber;
+		break;
+		case '-':  result = firstNumber - secondNumber;
+		break;
+		case '*':  result = firstNumber * secondNumber;
+		break;
+		case '/':  result = firstNumber / secondNumber;
+		break;
+	}
+
+	return `${expression} = ${result}`;
+}
+
+console.log(calc('1 * 2'));
