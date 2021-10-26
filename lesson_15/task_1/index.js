2d9876c8451dec5b23419ba8c08f46ab87a25446
Artyom Cheremisin <art.cheremisin@gmail.com>
2021-10-26 23:03:55 +0300

    Counter function - example of closure

---
+++ lesson_15/task_1/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+export function makeCounter(){
+	let count = 0;
+
+	return function(){
+		return count++;
+	}
+}
+
+const counter1 = makeCounter();
+const counter2 = makeCounter();
+
+console.log(counter1());
+console.log(counter1());
+console.log(counter1());
+console.log(counter2());
+
+
+
