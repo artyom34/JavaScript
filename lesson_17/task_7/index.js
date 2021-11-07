e9309f305434412d4d2a20bc878754b55c172d53
Artyom Cheremisin <art.cheremisin@gmail.com>
2021-11-07 13:51:37 +0200

    .bind in action

---
+++ lesson_17/task_7/index.js
@@ -0,0 +1,48 @@
+export function calculator(a, b) {
+  switch (this.operation) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '/':
+      return a / b;
+    case '*':
+      return a * b;
+    default:
+      return NaN;
+  }
+}
+
+/*
+ * Ф-ция multiplier должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 2 числа и возвращать из произведение
+ */
+
+// put your code here
+const multi = {
+	operation: '*',
+}
+
+export const multiplier = calculator.bind(multi);
+/*
+ * Ф-ция summator должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 2 числа и возвращать из сумму
+ */
+
+// put your code here
+const sum = {
+	operation: '+',
+}
+
+export const summator = calculator.bind(sum);
+/*
+ * Ф-ция twice должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 1 число и возвращать это число умноженное на 2
+ */
+
+// put your code here
+
+export const twice = calculator.bind(multi, 2);
