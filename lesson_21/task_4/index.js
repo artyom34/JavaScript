bde0766fcb0fa3e6962a39a00806067b17ab8ca1
Artyom Cheremisin <art.cheremisin@gmail.com>
2021-11-21 21:28:14 +0200

    Retrieving the content of an element

---
+++ lesson_21/task_4/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+export function getTitle(){
+	const elementTitle = document.querySelector('.title').textContent;
+
+	return elementTitle;
+}
+
+export function getDescription(){
+	const elementAbout = document.querySelector('.about').innerText;
+
+	return elementAbout;
+}
+
+export function getPlans(){
+	const elementPlan = document.querySelector('.plans').innerHTML;
+
+	return elementPlan;
+}
+
+export function getGoal(){
+	const elementGoal = document.querySelector('.goal').outerHTML;
+
+	return elementGoal;
+}
+
+const content = getGoal();
+
+console.log(content);
