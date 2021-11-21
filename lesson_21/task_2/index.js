3b29be161c1f809837f3de89f49b2311232a5e64
Artyom Cheremisin <art.cheremisin@gmail.com>
2021-11-21 14:14:57 +0200

    Search for an element on a page

--- lesson_21/task_2/index.js
+++ lesson_21/task_2/index.js
@@ -1,13 +1,16 @@
 'use strict';
 
-export function getTitleElement(){
-	const titleElem = document.querySelector('.title');
+function getTitleElement(){
+	const titleElem = document.querySelector('title');
 
 	return console.dir(titleElem);
 }
 
-export function getInputElement(){
-	const inputElem = document.querySelector('.text');
+function getInputElement(){
+	const inputElem = document.querySelector('input[type = text');
 
 	return console.dir(inputElem);	
 }
+
+getTitleElement();
+getInputElement();
