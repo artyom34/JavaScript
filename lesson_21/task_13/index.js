ff4bf9f485c37168d84db13e40d29f69358634bf
Artyom Cheremisin <art.cheremisin@gmail.com>
2021-11-24 07:54:12 +0200

    Find the closest parent

---
+++ lesson_21/task_13/index.js
@@ -0,0 +1,12 @@
+
+ function getSection(num){
+
+	const element = document.querySelector(`span[data-number= '${num}']`); 	
+	const parentElem = element.closest('.box');
+
+	return parentElem.dataset.section;
+ }
+
+let elem = getSection(2);
+
+console.log(elem);
